0994b7a706d5448de3b9341bf05825ee2f6fb837
Vadim-Cr <vadim.crudu@gmail.com>
2023-05-12 00:23:00 +0200

    finish

--- logic.js
+++ logic.js
@@ -4,32 +4,36 @@
 
 
 // Visualizzare in pagina 5 numeri casuali.
+// creo un array per i 5 numeri
+const myArr = [];
+const userArr = [];
     // creare 5 numeri casuali da 1 a 100
     // usare un ciclo for
      for (let i = 0; i < 5; i++ ) {
         let randomNum = getRandomInt(1, 100);
         console.log(randomNum);
+        myArr.push(randomNum);
         let rememberNum = document.createElement("p");
         rememberNum.innerText = randomNum;
         myContainer.append(rememberNum);
     }
-
-    // Devo creare una funzione con dentro un elemento p che dica "Spero tu abbia una buona" e sostituisca attrqverso innerHTML quello che c'era prima
-
+    console.log(myArr);
 
     // Da lì parte un timer di 30 secondi.
-let timer = setTimeout(rememberMe, 3000);
+let timer = setTimeout(rememberMe, 30000);
 
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
     // creamo un prompt dove l'utente inserisce i numeri 5 volte
     for (let i=0; i < 5; i++) {
-        let userTimer = setTimeout(userInput, 3100);
-        let 
-        console.log();
-    }
+        let userTimer = setTimeout(userInput, 31000);
+    };
 
 
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+setTimeout(() => {
+    compareArrays(myArr, userArr);
+  }, 3100 * 10);
+  
 
 // ! FUNCTIONS
 
@@ -40,11 +44,28 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
+// Devo creare una funzione con dentro un elemento p che dica "Spero tu abbia una buona" e sostituisca attrqverso innerHTML quello che c'era prima
 function rememberMe () {
-    myContainer.innerHTML = "<p>Spero tu abbia una buona memoria</p>";
+    myContainer.innerHTML = "<p>Spero tu abbia una buona memoria, hai 30 secondi per inserire i numeri</p>";
 }
 
+
 function userInput () {
-    let userInput = prompt("inserisci il tuo numero");
-    userInput =
+    let userNumbInput = parseInt(prompt("inserisci il tuo numero, hai solo 30 secondi!"));
+    userArr.push(userNumbInput);
 }
+
+function compareArrays(myArr, userArr) {
+    let userMemNumbers = 0;
+    for (let i = 0; i < myArr.length; i++) {
+      if (userArr.includes(myArr[i])) {
+        userMemNumbers++;
+        myContainer.innerHTML = `Hai memorizzato corettamente ${userMemNumbers} numeri`;
+        console.log(userMemNumbers);
+        console.log(`Value ${myArr[i]} is included in the second array.`);
+      } else {
+        console.log(`Value ${myArr[i]} is not included in the second array.`);
+      }
+    }
+  }
+
